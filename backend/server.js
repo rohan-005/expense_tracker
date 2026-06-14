a4c4605c170b134b1df92a9cdc0b9f005bc663cf
rohan-005 <rohandhanerwal@gmail.com>
2026-06-14 19:53:04 +0530

    file_size_issue_fixed

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,14 @@ const io = socketIo(server, {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+    limit: "10mb"
+}));
+
+app.use(express.urlencoded({
+    limit: "10mb",
+    extended: true
+}));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));

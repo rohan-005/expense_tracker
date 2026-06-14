06a03c9db4b087466b6b128420ac307099e96727
rohan-005 <rohandhanerwal@gmail.com>
2026-06-14 15:54:04 +0530

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://expense-tracker-rnu1.onrender.com',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:5000',
+        target: 'https://expense-tracker-rnu1.onrender.com',
         ws: true,
         changeOrigin: true,
       },
